5ba00616d8b704b2d0bc4152c9b0a04cead0f7ea
seanmeager <seanmeager10@gmail.com>
2021-08-22 18:56:22 +0100

    started page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { loadDynamicBannerText } from '../components/banner';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+  loadDynamicBannerText();
+});
